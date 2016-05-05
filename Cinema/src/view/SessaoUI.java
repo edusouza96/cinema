b1ac9c1b9fcac72e93230abec05f4db00dabbe6d
Edu <Edu@EDU>
2016-05-05 08:17:23 -0300

    funções basicas da classes: filme, Sala, Sessao, Assento, Venda Inclusão do menu e da classe main relatorio por venda, sessão teste unitarios view/repositorios validação data exceptions filme/sala/sessao versão 0.9.6

--- Cinema/src/view/SessaoUI.java
+++ Cinema/src/view/SessaoUI.java
@@ -1,11 +1,14 @@
 package view;
 
+import exceptions.ObjectNullException;
 import java.text.ParseException;
 import java.util.Date;
+import java.util.InputMismatchException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import static javax.swing.JOptionPane.ERROR_MESSAGE;
+import static javax.swing.JOptionPane.WARNING_MESSAGE;
 import model.Filme;
 import model.Sala;
 import model.Sessao;
@@ -34,34 +37,47 @@ public class SessaoUI {
         this.lista = lista;
         this.listaFilmes = listaFilme;
         this.listaSalas = listaSala;
+        Filme filme= new Filme("Vingadores: Era de Ultron","ficção científica/Ação","Sequência do sucesso 'Os Vingadores', que reúne mais uma vez a equipe de super-heróis");
+        Sala sala = new Sala(200, 400);
+        try {
+            lista.adicionar(new Sessao(filme, sala, DateUtil.stringToHour("10:00")));
+        } catch (ParseException ex) {
+            System.out.println("Não tem erro aqui, pois sou que manipulo");
+        }
     }
     
     /**
      * metodo que contem as opções para execução dos procedimentos
      */
     public void executar() {
-        int opcao;
+        int opcao = 0;
         do{
             System.out.println(MenuUI.menuSessao());
-            opcao = Console.scanInt("Digite a opção  desejada: ");
-            switch(opcao){
-                case MenuUI.CADASTRAR:
-                    cadastrarSessao();
-                    break;
-                case MenuUI.LISTAR:
-                    mostrarSessoes();
-                    break;
-                case MenuUI.EDITAR:
-                    alterarSessao();
-                    break;
-                case MenuUI.DELETAR:
-                   deletarSessoes();
-                    break;
-                case MenuUI.SAIR:
-                    JOptionPane.showMessageDialog(null, "Retornando ao Menu Principal!");
-                    break;
-                default:
-                    JOptionPane.showMessageDialog(null, "Opção Invalida!", null, ERROR_MESSAGE);
+            try{
+                opcao = Console.scanInt("Digite a opção  desejada: ");
+                switch(opcao){
+                    case MenuUI.CADASTRAR:
+                        cadastrarSessao();
+                        break;
+                    case MenuUI.LISTAR:
+                        mostrarSessoes();
+                        break;
+                    case MenuUI.EDITAR:
+                        alterarSessao();
+                        break;
+                    case MenuUI.DELETAR:
+                       deletarSessoes();
+                        break;
+                    case MenuUI.SAIR:
+                        JOptionPane.showMessageDialog(null, "Retornando ao Menu Principal!");
+                        break;
+                    default:
+                        JOptionPane.showMessageDialog(null, "Opção Invalida!", null, ERROR_MESSAGE);
+                }
+            }catch(InputMismatchException ex){
+                JOptionPane.showMessageDialog(null, "Somente valor numérico", "Erro", ERROR_MESSAGE);
+            } catch (ObjectNullException ex) {
+                JOptionPane.showMessageDialog(null, "Filme Não Localizado", null, ERROR_MESSAGE);
             }
         }while(opcao != 0);
     }
@@ -70,23 +86,37 @@ public class SessaoUI {
      * metodo que coleta as informações da sessão e depois
      * coloca na lista do repositorio
      */
-    private void cadastrarSessao() {
-        exibirFilmes();
-        System.out.println("---------------------------------\n");
-        String nomeFilme = Console.scanString("Nome do Filme: ");
-        Filme filme = listaFilmes.consultarPorNome(nomeFilme);
-        exibirSalas();
-        System.out.println("---------------------------------\n");
-        int numeroSala = Console.scanInt("Numero da Sala: ");
-        Sala sala = listaSalas.consultarPorSala(numeroSala);
-        String horario = Console.scanString("Horario da Sessão: ");
-        Date hora;
-        try {
+    private void cadastrarSessao() throws ObjectNullException {
+        
+        try{
+            exibirFilmes();
+            System.out.println("---------------------------------\n");
+            String nomeFilme = "";
+            do{
+                nomeFilme = Console.scanString("Nome do Filme: ");
+                if(nomeFilme.equals(""))
+                    JOptionPane.showMessageDialog(null, "Campo Nome do Filme\nDeve ser Preenchido", "Campo Obrigatório", WARNING_MESSAGE );
+            }while(nomeFilme.equals(""));
+
+            Filme filme = listaFilmes.consultarPorNome(nomeFilme);
+            if(filme == null)
+                throw new ObjectNullException();
+
+            exibirSalas();
+            System.out.println("---------------------------------\n");
+            int numeroSala = Console.scanInt("Numero da Sala: ");
+            Sala sala = listaSalas.consultarPorSala(numeroSala);
+            String horario = Console.scanString("Horario da Sessão: ");
+            Date hora;
             hora = DateUtil.stringToHour(horario);lista.adicionar(new Sessao(filme,sala,hora));
             JOptionPane.showMessageDialog(null, "Sessão cadastrada com suceso");
+            
+        }catch(InputMismatchException ex){
+            JOptionPane.showMessageDialog(null, "Somente valor numérico", "Erro", ERROR_MESSAGE);
         } catch (ParseException ex) {
             JOptionPane.showMessageDialog(null, "Hora Inválida\n Favor digitar no Padrão HH:MM", "Operação cancelada", ERROR_MESSAGE);
         }
+        
              
     }
     /**
@@ -127,48 +157,68 @@ public class SessaoUI {
     /**
      * metodo responsavel para alterar dados da sessão
      */
-    private void alterarSessao(){
-        int sessaoAlterar = Console.scanInt("Código da sessão a alterar: ");
-        Sessao sessao = lista.consultarPorCodigo(sessaoAlterar);
-        System.out.println("Dados Atuais");
-        System.out.println(sessao);
-        String resposta = Console.scanString("Mudar o Filme: sim ou não ~> ");
-        if(resposta.equalsIgnoreCase("sim")){
-            exibirFilmes();
-            System.out.println("---------------------------------\n");
-            String nomeFilme = Console.scanString("Novo Filme para esta sessao: ");
-            sessao.setFilme(listaFilmes.consultarPorNome(nomeFilme));
-        }
-        resposta = Console.scanString("Mudar a Sala: sim ou não ~> ");
-        if(resposta.equalsIgnoreCase("sim")){
-            exibirSalas();
-            System.out.println("---------------------------------\n");
-            int numeroSala = Console.scanInt("Nova sala para esta sessao: ");
-            sessao.setSala(listaSalas.consultarPorSala(numeroSala));
-        }
-        resposta = Console.scanString("Mudar o Horario: sim ou não ~> ");
-        if(resposta.equalsIgnoreCase("sim")){
-            String hora = Console.scanString("Novo Horario para esta sessao: ");
-            try {
-                sessao.setHorario(DateUtil.stringToHour(hora));
-                JOptionPane.showMessageDialog(null, "Mudanças Concluida");
-            } catch (ParseException ex) {
-                JOptionPane.showMessageDialog(null, "Hora Inválida\n Favor digitar no Padrão HH:MM", "Operação cancelada", ERROR_MESSAGE);
+    private void alterarSessao() throws ObjectNullException{
+        try{
+            int sessaoAlterar = Console.scanInt("Código da sessão a alterar: ");
+            Sessao sessao = lista.consultarPorCodigo(sessaoAlterar);
+            if(sessao == null)
+                throw new ObjectNullException();
+            
+            System.out.println("Dados Atuais");
+            System.out.println(sessao);
+            String resposta = Console.scanString("Mudar o Filme: sim ou não ~> ");
+            if(resposta.equalsIgnoreCase("sim")){
+                exibirFilmes();
+                System.out.println("---------------------------------\n");
+                String nomeFilme = "";
+                do{
+                    nomeFilme = Console.scanString("Novo Filme para esta sessao: ");
+                    if(nomeFilme.equals(""))
+                        JOptionPane.showMessageDialog(null, "Campo Nome do Filme\nDeve ser Preenchido", "Campo Obrigatório", WARNING_MESSAGE );
+                }while(nomeFilme.equals(""));
+                Filme listaF = listaFilmes.consultarPorNome(nomeFilme);
+                if(listaF == null)
+                    throw new ObjectNullException();
+                sessao.setFilme(listaF);
             }
-        }
-           
-                
+            resposta = Console.scanString("Mudar a Sala: sim ou não ~> ");
+            if(resposta.equalsIgnoreCase("sim")){
+                exibirSalas();
+                System.out.println("---------------------------------\n");
+                int numeroSala = Console.scanInt("Nova sala para esta sessao: ");
+                Sala listaS = listaSalas.consultarPorSala(numeroSala);
+                if(listaS == null)
+                    throw new ObjectNullException();
+                sessao.setSala(listaS);
+            }
+            
+            resposta = Console.scanString("Mudar o Horario: sim ou não ~> ");
+            if(resposta.equalsIgnoreCase("sim")){
+                String hora = Console.scanString("Novo Horario para esta sessao: ");
+                sessao.setHorario(DateUtil.stringToHour(hora));
+            }
+            JOptionPane.showMessageDialog(null, "Mudanças Concluida");
+        }catch(InputMismatchException ex){
+            JOptionPane.showMessageDialog(null, "Somente valor numérico", "Erro", ERROR_MESSAGE);
+        }catch (ParseException ex) {
+            JOptionPane.showMessageDialog(null, "Hora Inválida\n Favor digitar no Padrão HH:MM", "Operação cancelada", ERROR_MESSAGE);
+        }    
     }
 
     /**
      * metodo que deleta uma sessão da lista
      */
-    private void deletarSessoes() {
-        int sessaoDeletar = Console.scanInt("Codigo da sessão a excluir: ");
-        Sessao sessao = lista.consultarPorCodigo(sessaoDeletar);
-        lista.remover(sessao);
-        JOptionPane.showMessageDialog(null, "Exclusão Concluida");
-                
+    private void deletarSessoes() throws ObjectNullException {
+        try{
+            int sessaoDeletar = Console.scanInt("Codigo da sessão a excluir: ");
+            Sessao sessao = lista.consultarPorCodigo(sessaoDeletar);
+            if(sessao == null)
+                throw new ObjectNullException();       
+            lista.remover(sessao);
+            JOptionPane.showMessageDialog(null, "Exclusão Concluida");
+        }catch(InputMismatchException ex){
+            JOptionPane.showMessageDialog(null, "Somente valor numérico", "Erro", ERROR_MESSAGE);
+        }    
     }
     
     
