189b488be909f3f5f911f5323e9907710eced877
Eduardo Souza <eduardo_souza96@hotmail.com>
2016-06-06 00:40:23 -0300

    Update bd.sql

--- bd.sql
+++ bd.sql
@@ -1,38 +1,69 @@
-CREATE TABLE assento (
-  codigoAssento INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  sessao_codigoSessao INTEGER UNSIGNED NOT NULL,
-  assentosLivres INTEGER UNSIGNED NOT NULL,
-  data DATE NULL,
-  PRIMARY KEY(codigoAssento)
-);
-
-CREATE TABLE filme (
-  codigoFilme INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  nomeFilme VARCHAR(100) NOT NULL,
-  genero VARCHAR(100) NOT NULL,
-  sinopse TEXT NULL,
-  PRIMARY KEY(codigoFilme)
-);
-
-CREATE TABLE sala (
-  numeroSala INTEGER UNSIGNED NOT NULL,
-  quantidadeAssento INT NULL,
-  PRIMARY KEY(numeroSala)
-);
-
-CREATE TABLE sessao (
-  codigoSessao INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  sala_numeroSala INTEGER UNSIGNED NOT NULL,
-  filme_codigoFilme INTEGER UNSIGNED NOT NULL,
-  horario DATE NOT NULL,
-  PRIMARY KEY(codigoSessao)
-);
-
-CREATE TABLE venda (
-  registroVenda INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  sessao_codigoSessao INTEGER UNSIGNED NOT NULL,
-  data DATE NOT NULL,
-  PRIMARY KEY(registroVenda)
-);
 
 
+CREATE TABLE IF NOT EXISTS `assento` (
+  `codigoAssento` int(10) unsigned NOT NULL auto_increment,
+  `sessao_codigoSessao` int(10) unsigned NOT NULL,
+  `assentosLivres` int(10) unsigned NOT NULL,
+  `data` date default NULL,
+  PRIMARY KEY  (`codigoAssento`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+
+CREATE TABLE IF NOT EXISTS `filme` (
+  `codigoFilme` int(10) unsigned NOT NULL auto_increment,
+  `nomeFilme` varchar(100) collate utf8_unicode_ci NOT NULL,
+  `genero` varchar(100) collate utf8_unicode_ci NOT NULL,
+  `sinopse` text collate utf8_unicode_ci,
+  PRIMARY KEY  (`codigoFilme`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `filme`
+--
+
+INSERT INTO `filme` (`codigoFilme`, `nomeFilme`, `genero`, `sinopse`) VALUES
+(1, 'filme 1', 'genero 1', 'sinopse 1'),
+(2, 'filme 2', 'genero 2', 'sinopse 2'),
+(3, 'filme 3', 'genero 3', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sala`
+--
+
+CREATE TABLE IF NOT EXISTS `sala` (
+  `numeroSala` int(10) unsigned NOT NULL,
+  `quantidadeSala` int(11) default NULL,
+  PRIMARY KEY  (`numeroSala`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `sala`
+--
+
+INSERT INTO `sala` (`numeroSala`, `quantidadeSala`) VALUES
+(100, 1000),
+(200, 2000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sessao`
+--
+
+CREATE TABLE IF NOT EXISTS `sessao` (
+  `codigoSessao` int(10) unsigned NOT NULL auto_increment,
+  `sala_numeroSala` int(10) unsigned NOT NULL,
+  `filme_codigoFilme` int(10) unsigned NOT NULL,
+  `horario` time NOT NULL,
+  PRIMARY KEY  (`codigoSessao`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+
+CREATE TABLE IF NOT EXISTS `venda` (
+  `registroVenda` int(10) unsigned NOT NULL auto_increment,
+  `sessao_codigoSessao` int(10) unsigned NOT NULL,
+  `data` date NOT NULL,
+  PRIMARY KEY  (`registroVenda`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
